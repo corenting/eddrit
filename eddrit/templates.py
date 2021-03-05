9f8c438245fbf92056bae9f177623987a48238c7
Corentin Garcia <cgarcia@meilleursagents.com>
2021-03-05 18:04:35 +0100

    fix dependencies

--- eddrit/templates.py
+++ eddrit/templates.py
@@ -1,13 +1,12 @@
-from importlib.metadata import version
-
 from starlette.templating import Jinja2Templates
 
+from eddrit import __version__
 from eddrit.utils.subreddit import is_homepage
 
 templates = Jinja2Templates(directory="templates")
 
 # Add global informations to env
 templates.env.globals["global"] = {
-    "app_version": version("eddrit"),
+    "app_version": __version__,
     "subreddit_is_homepage": is_homepage,
 }
